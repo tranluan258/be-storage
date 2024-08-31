0d163c55ece4f508e320c2375ead0b6fece2e6e4
LuanTran <tranluanqqq@gmail.com>
2024-08-31 13:38:13 +0700

    feat(swagger): add swagger config

--- main/src/main.ts
+++ main/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('/api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('File Storage')
+    .setDescription('API document file storage')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/swagger', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('port') || 3000;
+
+  await app.listen(PORT);
 }
 bootstrap();
