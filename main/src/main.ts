cf2690e2283084c30b8441194a32ec08cfc055e6
LuanTran <tranluanqqq@gmail.com>
2024-09-14 13:30:12 +0700

    feat(deploy): add custom css and js for swagger ui

--- main/src/main.ts
+++ main/src/main.ts
@@ -20,6 +20,15 @@ async function bootstrap(): Promise<void> {
       tagsSorter: 'alpha',
       operationsSorter: 'alpha',
     },
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
   });
 
   const configService = app.get(ConfigService);
